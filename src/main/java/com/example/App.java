66ae0c99558dd53cc725affd36e06a6a99067904
lai2-24 <lai2-24@itismeucci.com>
2023-12-13 13:37:30 +0100

    senBinaryFile

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,6 +3,9 @@ package com.example;
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -15,38 +18,55 @@ public class App {
 
             ServerSocket server = new ServerSocket(3000);
 
-            Socket s = server.accept();  
-            BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
-            DataOutputStream out = new DataOutputStream(s.getOutputStream());
+            while(true) {
 
-            String line;
-            line = in.readLine();
+                Socket socket = server.accept();  
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                DataOutputStream out = new DataOutputStream(socket.getOutputStream());
 
-            String[] stringhe = line.split(" ");
-            String path = stringhe[1].substring(1);
-                
-            do{
-
+                String line;
                 line = in.readLine();
                 System.out.println("La string è: " + line);
 
-            }while(!line.isEmpty());
+                String[] stringhe = line.split(" ");
+                String path = stringhe[1].substring(1);
+                    
+                do{
 
-            File file = new File(path);
-            boolean flag = file.exists();
+                    line = in.readLine();
+                    System.out.println("La string è: " + line);
 
-            if(flag) {
+                }while(!line.isEmpty());
 
-                System.out.println("File trovato");
+                File file = new File(path);
+                boolean flag = file.exists();
+
+                if(flag) {
+
+                    String msg = "File trovato";
+                    System.out.println("msg");
+
+                    out.writeBytes("HTTP/1.1 200 OK\n");
+                    out.writeBytes("Content-Lenght: " + msg.length() + '\n');
+                    out.writeBytes("\n");
+                    out.writeBytes(msg);
+
+                }
+
+                else {
+
+                    String msg = "File non trovato";
+                    System.out.println("msg");
+
+                    out.writeBytes("HTTP/1.1 400 Not found\n");
+                    out.writeBytes("Content-Lenght: " + msg.length() + '\n');
+                    out.writeBytes("\n");
+                    out.writeBytes(msg);
+
+                }
+
+                socket.close();
             }
-
-            else {
-
-                System.out.println("File non trovato");
-            }
-            
-            s.close();
-            server.close();
 
         } 
 
@@ -56,6 +76,31 @@ public class App {
             System.exit(1);
         }
 
+    }
+
+    private static void sendBinaryFile(Socket socket, File file) throws IOException{
+
+        DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+        output.writeBytes("HTTP/1.1 200 OK\n");
+        output.writeBytes("Contenet-Lenght: " + file.length() + "\n");
+
+        output.writeBytes("Content-Type: " + getContentType(file) + "\n");
+
+        output.writeBytes("\n");
+        InputStream input = new FileInputStream(file);
+        byte[] buf = new byte[8192];
+        int n;
+
+        while((n = input.read(buf)) != -1) {
+
+            output.write(buf, 0, n);;
+        }
+        input.close();
+    
+    }
+
+    private static String getContentType(File file) {
+        return null;
     }
 
 }
